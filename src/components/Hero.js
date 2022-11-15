5a6dd9857911095275296c1e6043810e5a97033c
KIVTVN <tristan.garni@gmail.com>
2022-11-15 14:14:27 +0100

    boiler plate settings

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../assets/logo.png';
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <img src={logo} alt="Wolgang Museum logo" />
+    </div>
+  );
+};
+
+export default Hero;
